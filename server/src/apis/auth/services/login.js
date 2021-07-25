2082de8598edac5666ff21156784d513392cd78d
phamquyetthang <quyetthang.phucchi@gmail.com>
2021-07-25 22:53:27 +0700

    hash password

--- server/src/apis/auth/services/login.js
+++ server/src/apis/auth/services/login.js
@@ -4,15 +4,16 @@ import HttpException from '../../../common/helpers/HttpException';
 import userSchema from '../../../schema/userSchema';
 
 export default async function loginService(email, password) {
-  const user = await userSchema.findOne({
-    email: email,
-    password: password,
-  });
-  if (user) {
-    return {
-      token: jwt.sign(user.id, '123'),
-    };
+  const user = await userSchema.findOne({ email: email });
+  if (!user) {
+    throw new HttpException(409, `Email is not exist!`);
   } else {
-    throw new HttpException(400, 'dang nhap that bai');
+    const isMatchPass = await bcrypt.compare(password, user.password); // kiem tra bang nhau :)))
+    if (!isMatchPass) throw new HttpException(400, "Wrong password");
+    else {
+      return {
+        token: jwt.sign(user.id, '123'),
+      };
+    }
   }
 }
--- server/src/apis/auth/services/register.js
+++ server/src/apis/auth/services/register.js
@@ -10,18 +10,25 @@ export default async function registerService(
   avatar
 ) {
   try {
-    const user = await userSchema.create({
-      email,
-      password,
-      firstName,
-      lastName,
-      avatar,
-      info: '',
-    });
+    const user = await userSchema.findOne({ email: email });
+    if (user) {
+      throw new HttpException(409, `Your email ${email} already exist.`);
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const user = await userSchema.create({
+        email,
+        password: hashedPassword,
+        firstName,
+        lastName,
+        avatar,
+        info: '',
+      });
 
-    return {
-      token: jwt.sign(user.id, '123'),
-    };
+      return {
+        token: jwt.sign(user.id, '123'),
+      };
+    }
   } catch (error) {
     throw new HttpException(400, error.message);
   }
