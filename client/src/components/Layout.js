bd8f725a88be310b8fce84c56ddbe9f42b927b52
phamquyetthang <quyetthang.phucchi@gmail.com>
2021-07-29 17:32:44 +0700

    add home page

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,18 +1,29 @@
-import Head from "next/head";
+import Head from 'next/head';
+import Image from 'next/image';
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.svg" />
         <title>CS Forum</title>
       </Head>
       <main>
-      {children}
-      </main>
+        <header>
+          <div className="header-logo">
+            <Image alt="logo cs-tech" src="/logo.jpg" width={180} height={60} />
+          </div>
+        </header>
 
+        <article className="container">
+          {children}
+        </article>
+
+        <footer></footer>
+      </main>
     </div>
   );
 };
