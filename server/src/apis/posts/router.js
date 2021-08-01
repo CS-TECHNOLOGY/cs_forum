d569bdc874c309f8ee48aa5b1c86009738b37cd7
sonDA1 <maxseo9x@gmail.com>
2021-08-01 20:30:38 +0700

    ppush edit comment

--- server/src/apis/posts/router.js
+++ server/src/apis/posts/router.js
@@ -6,6 +6,7 @@ import likePostController from './controller/likePostController';
 import getNewFeedController from './controller/getNewfeed';
 import getPostController from './controller/getPost';
 import sharePostController from './controller/sharePost';
+import editCommentController from './controller/editComment';
 
 const postRouter = express.Router();
 const pathPost = '/post';
@@ -16,5 +17,6 @@ postRouter.post(`${pathPost}/comment`, authMiddleware, addCommentController);
 postRouter.post(`${pathPost}/like_post`, authMiddleware, likePostController);
 postRouter.get(`${pathPost}/newFeed/:pageNum`, getNewFeedController);
 postRouter.post(`${pathPost}/share_post`, authMiddleware, sharePostController);
+postRouter.post(`${pathPost}/edit_comment`, authMiddleware, editCommentController);
 
 export default postRouter;
